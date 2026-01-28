eb52ca8411da0c78ade9d33279d1cce61b681942
Daniel Doblas <dandobjim@gmail.com>
2026-01-28 16:54:42 +0100

    fix: fix lint

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -15,6 +15,7 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
     tags = TagSerializer(many=True, required=False)
+
     class Meta:
         model = Recipe
         fields = ["id", "title", "time_minutes", "price", "link", "tags"]
